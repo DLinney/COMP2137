0fb89050ea1cc4af7de6b8951a9a6f3699c54f51
DLinney <157623553+DLinney@users.noreply.github.com>
2024-03-30 12:29:32 -0400

    Update lab3.sh

--- lab3.sh
+++ lab3.sh
@@ -1,26 +1,74 @@
-#!/bin/bash
+!/bin/bash
 
-# Check if -verbose option is provided
-if [ "$1" = "-verbose" ]; then
-    verbose=true
-    shift
-else
-    verbose=false
-fi
+# Function to display usage information
+usage() {
+    echo "Usage: $0 [-v]"
+    echo "  -v: Enable verbose mode"
+    exit 1
+}
 
-# Copy configure-host.sh to remote servers
+# Default settings
+VERBOSE=false
+
+# Parse command line options
+while getopts ":v" opt; do
+    case ${opt} in
+        v)
+            VERBOSE=true
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND -1))
+
+# Transfer configure-host.sh script to server1-mgmt
 scp configure-host.sh remoteadmin@server1-mgmt:/root
-scp configure-host.sh remoteadmin@server2-mgmt:/root
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to transfer configure-host.sh to server1-mgmt"
+    exit 1
+fi
+sudo chmod +x configure-host.sh
 
-# Run configure-host.sh on remote servers
-if [ "$verbose" = true ]; then
-    ssh remoteadmin@server1-mgmt -- /root/configure-host.sh -verbose -name loghost -ip 192.168.16.3 -hostentry webhost 192.168.16.4
-    ssh remoteadmin@server2-mgmt -- /root/configure-host.sh -verbose -name webhost -ip 192.168.16.4 -hostentry loghost 192.168.16.3
-else
-    ssh remoteadmin@server1-mgmt -- /root/configure-host.sh -name loghost -ip 192.168.16.3 -hostentry webhost 192.168.16.4
-    ssh remoteadmin@server2-mgmt -- /root/configure-host.sh -name webhost -ip 192.168.16.4 -hostentry loghost 192.168.16.3
+# Run configure-host.sh script on server1-mgmt
+ssh_command="ssh remoteadmin@server1-mgmt"
+if [ "$VERBOSE" = true ]; then
+    ssh_command+=" --verbose"
+fi
+$ssh_command -- /root/configure-host.sh -name loghost -ip 192.168.16.200 -hostentry webhost 192.168.16.201
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to run configure-host.sh on server1-mgmt"
+    exit 1
 fi
 
-# Update local /etc/hosts
-./configure-host.sh -hostentry loghost 192.168.16.3
-./configure-host.sh -hostentry webhost 192.168.16.4
+# Transfer configure-host.sh script to server2-mgmt
+scp configure-host.sh remoteadmin@server2-mgmt:/root
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to transfer configure-host.sh to server2-mgmt"
+    exit 1
+fi
+
+# Run configure-host.sh script on server2-mgmt
+$ssh_command -- /root/configure-host.sh -name webhost -ip 192.168.16.201 -hostentry loghost 192.168.16.200
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to run configure-host.sh on server2-mgmt"
+    exit 1
+fi
+
+# Update local /etc/hosts file
+./configure-host.sh -hostentry loghost 192.168.16.200
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to update local /etc/hosts file for loghost"
+    exit 1
+fi
+
+./configure-host.sh -hostentry webhost 192.168.16.201
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to update local /etc/hosts file for webhost"
+    exit 1
+fi
+
+echo "Configuration applied successfully"
+exit 0
+
